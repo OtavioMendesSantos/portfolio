08d5ffa0325480b7351d8447466e7d8dc4aaaa3b
Otávio <otaviomendessantos2019@gmail.com>
2026-06-16 17:38:36 -0300

    fix: Update astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   integrations: [react()],
   vite: {
+    ssr: {
+      noExternal: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled', 'react-transition-group'],
+    },
     optimizeDeps: {
       exclude: ['react/jsx-dev-runtime', 'react/jsx-runtime'],
     },
