2d25c89e3670a2b336db842cb9079ce6a3cd6450
Otávio <otaviomendessantos2019@gmail.com>
2026-06-16 17:43:00 -0300

    fix: Update astro.config 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,29 @@
-import react from '@astrojs/react';
-import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   integrations: [react()],
   vite: {
+    resolve: {
+      alias: {
+        "react-transition-group/TransitionGroupContext":
+          "react-transition-group/TransitionGroupContext.js",
+      },
+    },
     ssr: {
-      noExternal: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled', 'react-transition-group'],
+      noExternal: [
+        "@mui/material",
+        "@mui/icons-material",
+        "@emotion/react",
+        "@emotion/styled",
+        "@mui/system",
+        "@mui/base",
+        "@mui/utils",
+        "react-transition-group",
+      ],
     },
     optimizeDeps: {
-      exclude: ['react/jsx-dev-runtime', 'react/jsx-runtime'],
+      exclude: ["react/jsx-dev-runtime", "react/jsx-runtime"],
     },
   },
 });
